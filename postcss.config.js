f235106dc1824298dd09ad60aad733032a59510d
ateszki <ateszki@gmail.com>
2020-03-26 08:32:40 -0300

    maqueta inicial

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const tailwindcss = require('tailwindcss');
+ module.exports = {
+     plugins: [
+         require('tailwindcss'),
+         require('autoprefixer'),
+         process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+              content: [
+                './src/**/index.html',
+              ],
+              defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+            }),
+        process.env.NODE_ENV === 'production' &&  require('cssnano')({
+            preset: 'default',
+        }),
+     ],
+ };
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+        width: {
+            '320p': '320px',
+            '660p': '660px',
+            '1000p': '1000px'
+        },
+        height: {
+            '50p': '50px',
+            '450p': '450px',
+        }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
