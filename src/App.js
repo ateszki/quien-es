47017509f4bfa24573423db72711838ade339cc4
ateszki <ateszki@gmail.com>
2020-03-28 23:02:43 -0300

    version 1

--- src/App.js
+++ src/App.js
@@ -4,42 +4,50 @@ import Card from "./Card.js";
 
 class App extends Component{
   constructor(props) {
-    console.log(preguntas)
     super(props);
     this.state = {
         preguntas: preguntas,
-        ayudas: 3,
-        respuestas: {}
+        respuestas: []
     }
+    this.shareLink = this.shareLink.bind(this);
+    this.shareText = this.shareText.bind(this);
     this.posibles = this.posibles.bind(this);
     this.puntos = this.puntos.bind(this);
+    this.handleRespuesta = this.handleRespuesta.bind(this);
   }
 
   posibles(){
-    return '200';
+    return this.state.preguntas.futbol.map(v=>{return v.dificultad * 2}).reduce(function(t,v){ return t + v;});
   }
   puntos(){
-    return '000';
+    var puntos = this.state.respuestas.length == 0 ? 0 : this.state.respuestas.map(r => { return preguntas.futbol[r].dificultad * 2}).reduce(function(t,v){ return t + v;});
+
+    return puntos.toString().padStart(3,'0');
+  }
+  handleRespuesta(index){
+    var respuestas = this.state.respuestas;
+    respuestas.push(index);
+    this.setState({respuestas: respuestas});
+  }
+  shareText(){
+    return encodeURI("¡Jugá a adivinar los futbolistas! ¡¡Yo ya hice "+ this.puntos()+"!! https://juego-quien-es.web.app/");
+  
+  }
+  shareLink(){
+    return "https://juego-quien-es.web.app/";
+  
   }
   render(){
     return(
         <div>
-            <div className="sticky top-0 z-50 mt-8 mx-auto w-320p md:w-660p lg:w-1000p rounded border-2 border-solid border-teal-600 bg-teal-600 text-white text-center shadow-md">
+            <div className="sticky top-0 z-50 mt-8 mx-auto w-320p md:w-660p lg:w-1000p rounded border-2 border-solid border-green-600 bg-green-600 text-white text-center shadow-md">
               <div className="text-2xl font-bold">PUNTOS: {this.puntos()} / {this.posibles()}</div>
-              <div className="py-2 text-sm md:text-lg ">Ayudas restantes: {Array(this.state.ayudas).map((value, index) => { return '🚑'; })}</div>
-              <div className="py-2 text-sm md:text-lg">Compartir: <a className="bg-green-400 hover:bg-green-500 text-white font-bold p-1 rounded inline-flex items-center" href=""><img className="fill-current w-4 h-4" src="./img/whatsapp.svg" /></a>&nbsp;<a className="bg-blue-600 hover:bg-blue-800 text-white font-bold p-1 rounded inline-flex items-center" href=""><img className="fill-current w-4 h-4" src="./img/facebook.svg" /></a>&nbsp;<a className="bg-blue-300 hover:bg-blue-500 text-white font-bold p-1 rounded inline-flex items-center" href=""><img className="fill-current w-4 h-4" src="./img/twitter.svg" /></a>&nbsp;<a className="bg-orange-300 hover:bg-orange-500 text-white font-bold p-1 rounded inline-flex items-center" href=""><img className="fill-current w-4 h-4" src="./img/instagram.svg" /></a></div>
+              <div className="py-2 text-sm md:text-lg">Compartir: <a className="md:hidden bg-green-400 hover:bg-green-500 text-white font-bold p-1 rounded inline-flex items-center" target="_blank" href={`https://api.whatsapp.com/send??text=${this.shareText()}`}><img className="fill-current w-4 h-4" src="./img/whatsapp.svg" /></a>&nbsp;<a className="bg-blue-600 hover:bg-blue-800 text-white font-bold p-1 rounded inline-flex items-center" target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${this.shareLink()}`}><img className="fill-current w-4 h-4" src="./img/facebook.svg" /></a>&nbsp;<a className="bg-blue-300 hover:bg-blue-500 text-white font-bold p-1 rounded inline-flex items-center" target="_blank" href={`https://twitter.com/intent/tweet?text=${this.shareText()}`}><img className="fill-current w-4 h-4" src="./img/twitter.svg" /></a></div>
             </div>
             <div className="mt-2 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mx-auto w-320p md:w-660p lg:w-1000p">
-              { this.state.preguntas.futbol.map((pregunta,index) => {return <Card key={index} pregunta={pregunta} /> })}
+              { this.state.preguntas.futbol.map((pregunta,index) => {return <Card key={index} index={index} onRespuestaCorrecta={this.handleRespuesta} pregunta={pregunta} /> })}
 
-              <div className="relative rounded-lg border-4 border-solid border-green-600 w-320p h-450p" style={{background: `url(./img/futbol/01.jpg) 0 0 no-repeat`}}>
-                <div className="w-full bg-green-600 text-white font-bold h-8 pt-1 pr-1 grid grid-cols-4 gap-0">
-                  <div className="pl-1"><span className="inline-block h-5 w-5 border border-white border-solid text-center text-xs">✓</span></div><div className="text-right col-span-3">Dificultad: 🔥🔥🔥🔥</div>
-                </div>
-                <div className="bottom-0 absolute h-50p bg-green-600 w-full">
-                  Diablo Etcheverry
-                </div>
-              </div>
+
 
 
 
--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,69 @@
 import React, { Component} from "react";
 
-
 class Card extends Component{
   constructor(props) {
     super(props);
     this.state = {
         value: '',
+        index: (this.props.index+1).toString().padStart(2,'0'),
+    }
+              
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.esCorrecto = this.esCorrecto.bind(this);
+    this.ayuda = this.ayuda.bind(this);
+    this.dificultad = this.dificultad.bind(this);
+    this.enviarResultado = this.enviarResultado.bind(this);
+  }
+  handleChange(event){
+    this.setState({value: event.target.value}, this.enviarResultado);
+    
+  }
+  enviarResultado(){
+    if(this.esCorrecto()){
+        this.props.onRespuestaCorrecta(this.props.index);
     }
   }
+  esCorrecto(){
+    return this.props.pregunta.nombre.toLowerCase() == this.state.value.toLowerCase();
+  }
+
+  handleClick(event){
+    this.ayuda();
+  }
+  dificultad(){
+    var d = '';
+    for(var i=0;i < this.props.pregunta.dificultad; i++){
+        d = d + '⚽️';//
+    } 
+    return d;
+  }
+  ayuda(){
+    var pregunta = this.props.pregunta.nombre;
+    var terms = pregunta.split(" ");
+    var ayudaStr = terms.length == 1 ? pregunta.substr(0,3) : terms[0];
+    this.setState({value: ayudaStr });
+  }
   render(){
+    const correcto = this.esCorrecto();
+
+    let button;
+
+    if(correcto){
+        //button = <img src="./img/checkmark.svg" className="fill-current text-white inline-block h-5 w-5" />;
+        button = <svg className="fill-current text-white inline-block h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>;
+    } else {
+        button = <button className="flex-shrink-0 bg-none border-none text-md py-1 px-2" type="button" onClick={this.handleClick}>🚑</button>;
+    }
+
     return(
-        <div className="relative rounded-lg border-4 border-solid border-green-600 w-320p h-450p" style={{background: `url(./img/futbol/01.jpg) 0 0 no-repeat`}}>
-            <div className="w-full bg-green-600 text-white font-bold h-8 pt-1 pr-1 grid grid-cols-4 gap-0">
-              <div className="pl-1"><span className="inline-block h-5 w-5 border border-white border-solid text-center text-xs">✓</span></div><div className="text-right col-span-3">Dificultad: 🔥🔥🔥🔥</div>
-            </div>
-            <div className="bottom-0 absolute h-50p bg-green-600 w-full">
-              {this.props.pregunta.nombre}
+        <div className={`relative rounded-lg border-4 border-solid ${this.esCorrecto() ? 'border-green-600' : 'border-red-600'} w-320p h-450p`} style={{background: `url(./img/futbol/${this.state.index}.jpg) 0 0 no-repeat`}}>
+            <div className={`w-full ${this.esCorrecto() ? 'bg-green-600' : 'bg-red-600'} text-white text-center font-bold h-8 pt-1 pr-1` }>Dificultad: {this.dificultad()}</div>
+            <div className={`bottom-0 absolute h-50p ${this.esCorrecto() ? 'bg-green-600' : 'bg-red-600'} w-full`}>
+              <div className="flex items-center border-b border-b-2 border-white pt-1 px-1">
+                <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" value={this.state.value} onChange={this.handleChange} placeholder="Escribe aquí la respuesta" />
+                {button}
+              </div>
             </div>
         </div>
     );
