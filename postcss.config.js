1fa62876b264c6c3e71ab8846ec5754a073ead82
ateszki <ateszki@gmail.com>
2020-03-28 23:12:02 -0300

    version 1

--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,12 @@ const tailwindcss = require('tailwindcss');
      plugins: [
          require('tailwindcss'),
          require('autoprefixer'),
-         process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+         /*process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
               content: [
                 './src/*.js',
               ],
               defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-            }),
+            }),*/
         process.env.NODE_ENV === 'production' &&  require('cssnano')({
             preset: 'default',
         }),
